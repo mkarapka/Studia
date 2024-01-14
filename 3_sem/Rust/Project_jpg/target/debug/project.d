80635fcfd8e072cc95a6f699abc62450adf717e4
Mikol <mikolaj.karapka@gmail.com>
2024-01-14 23:50:27 +0100

    Delete some files

--- 3_sem/Rust/Project_jpg/target/debug/project.d
+++
@@ -1,1 +0,0 @@
-/home/mikol/Projects/Studia/3_sem/Rust/Project/target/debug/project: /home/mikol/Projects/Studia/3_sem/Rust/Project/src/complex_mod.rs /home/mikol/Projects/Studia/3_sem/Rust/Project/src/main.rs
